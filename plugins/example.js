d9bda391ae37e215226a780f70840c469545597f
ZeynXZ <118685103+zeyndvp@users.noreply.github.com>
2024-04-18 06:34:01 +0000

    Zeyn Bot

---
+++ plugins/example.js
@@ -0,0 +1,15 @@
+exports.run = {
+  command: ['example'],
+  hidden: [''],
+  use: 'string',
+  category: 'string',
+  async: async (m, {
+    client
+  }) => {
+    try {
+      // Code Here
+    } catch (e) {
+      // Error Handling
+    }
+  }
+}
